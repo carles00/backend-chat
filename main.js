c4f0d61857cadbb09ca6ad0b69ed3bd7ac5eac4b
Carles Gallardo <45995898+carles00@users.noreply.github.com>
2023-02-04 01:34:03 +0100

    wewbsocket server not working

--- main.js
+++ main.js
@@ -13,12 +13,31 @@ var webSocketUrl = process.argv[2] ? process.argv[2] : `https://ecv-etic.upf.edu
 const server = http.createServer( app );
 
 //initialize the WebSocket server instance
-const wss = new WebSocketServer({ httpServer: server });
+var wss = new WebSocketServer({ httpServer: server});
 
 app.use(express.static('public'));
+
 
 //to launch
 app.listen(port, function () {
 	console.log(`Chatter listening on port ${port}`);
 });
+
+
   
+wss.on('request', function(request) {
+	console.log(request);
+	var connection = request.accept(null, request.origin);
+  
+	// This is the most important callback for us, we'll handle
+	// all messages from users here.
+	connection.on('message', function(message) {
+	  if (message.type === 'utf8') {
+		// process WebSocket message
+	  }
+	});
+  
+	connection.on('close', function(connection) {
+	  // close user connection
+	});
+  });
