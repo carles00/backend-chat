73f431d875749583e6540f6459cd8f7365eca919
Carles Gallardo <45995898+carles00@users.noreply.github.com>
2023-02-18 20:06:08 +0100

    server redirects

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+// GET value from key in Redis DB
+redis.get('key', (err, value) => {
+  if (err) console.error(`Error getting key: ${err}`)
+  else console.log(`The key has the value: ${value}`)
+})
+
+// PUT key-value pair in Redis DB
+redis.put('key', 'value', err => {
+  if (err) console.error(`Error putting key: ${err}`)
+  else console.log('Key saved successfully!')
+})
+
+
+
+
 const onUserConnected = connection => {
   console.log(connection)
 }
